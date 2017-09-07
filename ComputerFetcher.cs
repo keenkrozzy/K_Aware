83ca53a5654213a0a7cd5be5eda9e75ab0cd30f2
KeenKrozzy <KeenKrozzy@gmail.com>
2017-09-07 12:50:31 -0400

    Initial Commit

---
+++ ComputerFetcher.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.DirectoryServices;
+
+namespace K_Aware
+{
+    public class ComputerFetcher
+    {
+        public static List<string> GetComputers()
+        {
+            List<string> computerNames = new List<string>();
+
+            using (DirectoryEntry entry = new DirectoryEntry("LDAP://andent.andersonsinc.com"))
+            {
+                using (DirectorySearcher mySearcher = new DirectorySearcher(entry))
+                {
+                    mySearcher.Filter = ("(objectClass=computer)");
+
+                    // No size limit, reads all objects
+                    mySearcher.SizeLimit = 0;
+
+                    // Read data in pages of 250 objects. Make sure this value is below the limit configured in your AD domain (if there is a limit)
+                    mySearcher.PageSize = 250;
+
+                    // Let searcher know which properties are going to be used, and only load those
+                    mySearcher.PropertiesToLoad.Add("name");
+
+                    foreach (SearchResult resEnt in mySearcher.FindAll())
+                    {
+                        // Note: Properties can contain multiple values.
+                        if (resEnt.Properties["name"].Count > 0)
+                        {
+                            string computerName = (string)resEnt.Properties["name"][0];
+                            computerNames.Add(computerName);
+                        }
+                    }
+                }
+            }
+
+            return computerNames;
+        }
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace K_Aware
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstbxTest = new System.Windows.Forms.ListBox();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstbxTest
+            // 
+            this.lstbxTest.FormattingEnabled = true;
+            this.lstbxTest.Location = new System.Drawing.Point(12, 12);
+            this.lstbxTest.Name = "lstbxTest";
+            this.lstbxTest.Size = new System.Drawing.Size(215, 524);
+            this.lstbxTest.TabIndex = 1;
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(309, 29);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(75, 23);
+            this.btnTest.TabIndex = 2;
+            this.btnTest.Text = "button1";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 553);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.lstbxTest);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstbxTest;
+        private System.Windows.Forms.Button btnTest;
+    }
+}
+
